834aaac95ff727d03302e7a2aaf887a2bf63bef4
Gedeon Kasereka <kasereka@kth.se>
2019-04-01 17:52:32 +0200

    refractoring - wrapping duplicate code into function

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,41 +1,33 @@
-var carPrice = 100000; 
-document.getElementById('price').innerHTML = carPrice;
-var interest = 0.0475;
+var carPrice = 100000; //set car price
+$('#price').html(carPrice); //dynamically display car price
+var interest = 0.0475; //set interest rate
 
 var months;
 var deposit;
 
+//default values
 $('#depositSlider').attr('max', carPrice/2);
 $('#depositSlider').attr('min', carPrice/10);
 $('#depositSlider').attr('value', carPrice/10);
-
 $('#depositValue').html(carPrice/10);
 
-// print initial results
-$('#total-price').html(computeTotalPrice(12, carPrice/10));
-$('#monthlyRepayment').html(computeMonthly(carPrice/10, 12));
+// print initial results with default input
+displayComputedValue(12, carPrice/10);
 
 $('#depositSlider').on('change', function(){
 	deposit = $('#depositSlider').val(); 
 	months = $('#monthSlider').val();
 
-	$('#depositValue').html(deposit);
-
-	$('#total-price').html(computeTotalPrice(months, deposit));
-	$('#monthlyRepayment').html(computeMonthly(deposit, months));
+	$('#depositValue').html(deposit); //print changed deposit value
+	displayComputedValue(months, deposit); //print computed plans
 });
 
 $('#monthSlider').on('change', function(){
-
-	deposit = $('#depositSlider').val(); 
+	deposit = $('#depositSlider').val();
 	months = $('#monthSlider').val();
 
-	// print changed monthy value
-	$('#monthValue').html(months);
-
-	// print results 
-	$('#total-price').html(computeTotalPrice(months, deposit));
-	$('#monthlyRepayment').html(computeMonthly(deposit, months));
+	$('#monthValue').html(months); //print changed monthy value
+	displayComputedValue(months, deposit); //print computed plans
 });
 
 function computeTotalPrice(month, deposited){
@@ -48,6 +40,10 @@ function computeMonthly(deposited, month){
 	return monthlyPayment;
 }
 
-// Show interest on page
-$('#interestRate').html(interest *100); 
+function displayComputedValue(month, deposited){
+	$('#total-price').html(computeTotalPrice(month, deposited));
+	$('#monthlyRepayment').html(computeMonthly(deposited, month));
+}
 
+// Show interest on page
+$('#interestRate').html(interest * 100);
